70ec9a6377c8c50ef0e942d458317585216e7ceb
Rahul Balashanmugam <tsca.rahul@outlook.com>
2025-03-31 13:12:45 +0100

    Files added

--- src/test/java/pages/ProductPage.java
+++ src/test/java/pages/ProductPage.java
@@ -1,6 +1,9 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import utility.BrowserDriver;
 
@@ -24,37 +27,42 @@ public class ProductPage extends BrowserDriver {
 
 	public static void click_View_Product_Icon() throws InterruptedException {
 		Thread.sleep(2000);
-		driver.findElement(By.xpath(view_product_icon)).click();
+		WebElement productLink = driver.findElement(By.xpath(view_product_icon));
+		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", productLink);
+		productLink.click();
 	}
 
 	public static void click_Check_Category() throws InterruptedException {
 		Thread.sleep(2000);
 		String product_icon = driver.findElement(By.xpath(check_Category)).getText();
 		assertEquals ("Category: Women > Tops", product_icon);
+		System.out.println("Category: " + product_icon);
 	}
 
 	public static void click_Check_Product_Name() throws InterruptedException {
 		Thread.sleep(2000);
 		String click_Check_Product_Name = driver.findElement(By.xpath(check_Product_Name)).getText();
 		assertEquals ("Blue Top", click_Check_Product_Name);
+		System.out.println("Clicked on Product Name: " + click_Check_Product_Name);
 	}
 
 	public static void click_Check_Availability() throws InterruptedException {
 		Thread.sleep(2000);
 		String click_Check_Availability = driver.findElement(By.xpath(check_Availability)).getText();
-		assertEquals ("Availability", click_Check_Availability);
+		assertEquals ("Availability:", click_Check_Availability);
+		System.out.println("Clicked on Availability:" + click_Check_Availability);
 	}
 
 	public static void click_Check_Condition() throws InterruptedException {
 		Thread.sleep(2000);
 		String click_Check_Condition = driver.findElement(By.xpath(check_Condition)).getText();
-		assertEquals ("Condition", click_Check_Condition);
+		assertEquals ("Condition:", click_Check_Condition);
 	}
 
 	public static void click_Check_Brand() throws InterruptedException {
 		Thread.sleep(2000);
 		String click_Check_Brand = driver.findElement(By.xpath(check_Brand)).getText();
-		assertEquals ("Brand", click_Check_Brand);
+		assertEquals ("Brand:", click_Check_Brand);
 	}
 
 	public static void click_Back() throws InterruptedException {
